9998a30da939bb33fbaea6b04c30dc9e9ca91252
Kumar Sharad <114303424+KnightRiderSharad@users.noreply.github.com>
2023-01-17 16:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // complete the given function
 
-// complete the given function
-
 function palindrome(str){
  var removeChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
 
@@ -14,7 +12,7 @@ function palindrome(str){
    }else{
      return false;
    }
-}
+
 
 
 module.exports = palindrome
